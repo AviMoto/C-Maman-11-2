10936bcb36e289e1c242812b7b8d2e58e67530e4
avi <avi@admintheweb.com>
2013-11-03 22:30:17 +0200

    Build make file and make some changes in code

--- src/translate_dec_hex.c
+++ src/translate_dec_hex.c
@@ -7,21 +7,26 @@
 
 #include "mamn11-2.h"
 
-void translate_dec_hex(char x[]){
+void translate_dec_hex(char x[]) {
 	char binary[NUMBER_OF_DIGIT_IN_BINARY_UINT];
 	unsigned int number, charIndex, binaryLength;
 
 	sscanf(x, "%u", &number);
-	for(charIndex = 0; number > 0; charIndex++){
-		binary[charIndex] = (number % 2) + '0';
-		number /= 2;
+	if (number != 0) {
+		for (charIndex = 0; number > 0; charIndex++) {
+			binary[charIndex] = (number % 2) + '0';
+			number /= 2;
+		}
+		binary[charIndex] = '\0';
+		binaryLength = strlen(binary);
+		for (charIndex = 0; charIndex < binaryLength / 2; ++charIndex) {
+			binary[charIndex] += binary[binaryLength - 1 - charIndex];
+			binary[binaryLength - 1 - charIndex] = binary[charIndex]
+					- binary[binaryLength - 1 - charIndex];
+			binary[charIndex] -= binary[binaryLength - 1 - charIndex];
+		}
+	} else {
+		strcpy(binary, "0");
 	}
-	binary[charIndex] ='\0';
-	binaryLength = strlen(binary);
-	for(charIndex = 0 ; charIndex < binaryLength/2 ; ++charIndex){
-		binary[charIndex] += binary[binaryLength-1-charIndex];
-		binary[binaryLength-1-charIndex] = binary[charIndex] - binary[binaryLength-1-charIndex];
-		binary[charIndex] -= binary[binaryLength-1-charIndex];
-	}
-	printf("%s", binary);
+	printf("%s\n", binary);
 }
